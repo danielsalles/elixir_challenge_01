2d3e61211eb3d4e3c51616d49c461e2dd75eab6e
Daniel Bastos <danielsalles@gmail.com>
2022-01-02 13:36:27 -0300

    Finish first exercise

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,7 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
-  end
+  def call(list), do: count(list, 0)
+
+  defp count([], acc), do: acc
+
+  defp count([_head | tail], acc), do: count(tail, acc + 1)
 end
